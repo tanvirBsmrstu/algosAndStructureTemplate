709cb4e7c13c07dafc64a50207bc9ac98d245c09
Tanvir Hasan <tanvir.bsmrstu@gmail.com>
2023-05-19 03:50:04 +0200

    Sparse Table codeforces

--- PracticeArena/sparseTable_cf_F.cpp
+++ PracticeArena/sparseTable_cf_F.cpp
@@ -1,94 +1,234 @@
 
 #include <iostream>
+#include <functional>
 
 using namespace std;
 
-const int LOG = 17;
-const int N = 3 + 2e5;
-pair<int, int> spTable[LOG][N];
+/////////////////////////////////////////////////////////////////////////////
+///             ACCEPTED
+///     https://codeforces.com/contest/1454/problem/F
+///
+///     from each index i calculate the answer for the bucket size of 2^j
+///     if the starting index L and R-L+1 = K
+///     answer lying in [L][2^(k-1)], [L+2^(k-1)][k-1]
+///////////////////////////////////////////////////////////////////////////////
 
-pair<int, int> f(pair<int, int> &a, pair<int, int> &b)
+const int N = 3 + 2e5;
+
+template <typename T>
+class SparseTable
 {
-        return make_pair(min(a.first, b.first), max(a.second, b.second));
-}
-int logb2[N];
-void buildSpTable(int n)
-{
-        for (int j = 1; j <= logb2[n]; j++)
+
+        vector<vector<T>> spTable;
+        vector<int> logb2;
+        void preCalculateLogBase2(size_t maxLen)
         {
-                for (int i = 0; i + (1 << j) <= n; i++)
+                logb2.resize(maxLen);
+                logb2[1] = 0;
+                for (int i = 2; i < maxLen; i++)
                 {
-                        spTable[j][i] = f(spTable[j - 1][i], spTable[j - 1][i + (1 << (j - 1))]);
+                        logb2[i] = logb2[i >> 1] + 1;
                 }
         }
-}
 
-pair<int, int> query(int low, int high)
+        T f(T &a, T &b)
+        {
+                return make_pair(min(a.first, b.first), max(a.second, b.second));
+        }
+
+public:
+        SparseTable(size_t maxLen)
+        { /// len of the array
+                preCalculateLogBase2(maxLen + 5);
+                spTable.resize(logb2[maxLen] + 2, vector<T>(maxLen));
+        }
+
+        void buildSpTable(int n)
+        {
+                for (int j = 1; j <= logb2[n]; j++)
+                {
+                        for (int i = 0; i + (1 << j) <= n; i++)
+                        {
+                                spTable[j][i] = f(spTable[j - 1][i], spTable[j - 1][i + (1 << (j - 1))]);
+                        }
+                }
+        }
+
+        T query_impodent(int low, int high) // f(a)= x, then f(f(a))=x :: impodent function
+        {
+                int len = high - low + 1;
+                if (low > high)
+                        make_pair(-1, -1);
+                int k = logb2[len];
+                return f(spTable[k][low], spTable[k][high - (1 << k) + 1]);
+        }
+
+        T directAccess(int from, size_t powLength)
+        {
+                return spTable[powLength][from];
+        }
+
+        void directRawInput(size_t len)
+        {
+                for (int i = 0; i < len; i++)
+                {
+                        cin >> spTable[0][i].first;
+                        spTable[0][i].second = spTable[0][i].first;
+                }
+        }
+        T directRawAccess(int at_index)
+        {
+                return spTable[0][at_index];
+        }
+};
+
+class BinarySearch
 {
-        int len = high - low + 1;
-        int k = logb2[len];
-        return f(spTable[k][low], spTable[k][high - (1 << k) + 1]);
-}
+public:
+        enum Comp
+        {
+                LEFT = -1,
+                EQUAL = 0,
+                RIGHT = 1
+        };
+        int lastOccurance(int low, int high, function<Comp(int)> comparetor)
+        {
+                int answer = -1;
+                int left = low, right = high;
+                while (low <= high)
+                {
+                        int mid = (low + high) >> 1;
+                        int comp_val = comparetor(mid);
+                        if (comp_val == EQUAL)
+                        {
+                                answer = mid;
+                                low = mid + 1;
+                        }
+                        else if (comp_val == LEFT) // go left
+                        {
+                                high = mid - 1;
+                        }
+                        else
+                        {
+                                low = mid + 1;
+                        }
+                }
+                return answer;
+        }
+        int firstOccurance(int low, int high, function<Comp(int)> comparetor)
+        {
+                int answer = -1;
+                int left = low, right = high;
+                while (low <= high)
+                {
+                        int mid = (low + high) >> 1;
+                        int comp_val = comparetor(mid);
+                        if (comp_val == EQUAL)
+                        {
+                                answer = mid;
+                                high = mid - 1;
+                        }
+                        else if (comp_val == LEFT) // go left
+                        {
+                                high = mid - 1;
+                        }
+                        else
+                        {
+                                low = mid + 1;
+                        }
+                }
+                return answer;
+        }
+};
 
 int main()
 {
         int n;
         int testCases;
-        logb2[1] = 0;
-        for (int i = 2; i < N; i++)
-        {
-                logb2[i] = logb2[i >> 1] + 1;
-        }
+        SparseTable<pair<int, int>> table(N);
+        BinarySearch bSearch;
         cin >> testCases;
         while (testCases--)
         {
                 cin >> n;
-                for (int i = 0; i < n; i++)
-                {
-                        cin >> spTable[0][i].first;
-                        spTable[0][i].second = spTable[0][i].first;
-                }
-                buildSpTable(n);
+                int found = false;
+
+                table.directRawInput(n);
+
+                table.buildSpTable(n);
+
                 int left = 0, right = n - 1;
-                while (left < right)
+                if (n == 3 && table.directRawAccess(0) == table.directRawAccess(1) && table.directRawAccess(0) == table.directRawAccess(2))
                 {
-                        int p1 = query(0, left).second;
-                        int p3 = query(right, n - 1).second;
-                        // cout << "left " << left << " right " << right << "p1 " << p1 << " p3 " << p3 << endl;
-                        if (p1 < p3)
-                                left++;
-                        else if (p1 > p3)
-                                right--;
-                        else
+                        found = true;
+                        cout << "YES\n1 1 1\n";
+                        continue;
+                }
+                for (int i = 0; i < n - 2; i++)
+                {
+                        left = i;
+                        int p1 = table.query_impodent(0, i).second;
+
+                        int R = bSearch.lastOccurance(i + 1, n - 2,
+                                                      [&table, &i, &p1](int mid) -> BinarySearch::Comp
+                                                      {
+                                                              int p = table.query_impodent(i + 1, mid).first;
+                                                              if (p == p1)
+                                                                      return BinarySearch::Comp::EQUAL;
+                                                              else if (p < p1)
+                                                                      return BinarySearch::Comp::LEFT; // min is in decreasing order
+                                                              else
+                                                                      return BinarySearch::Comp::RIGHT;
+                                                      });
+                        int L = bSearch.firstOccurance(i + 1, n - 2,
+                                                       [&table, &i, &p1](int mid) -> BinarySearch::Comp
+                                                       {
+                                                               int p = table.query_impodent(i + 1, mid).first;
+                                                               if (p == p1)
+                                                                       return BinarySearch::Comp::EQUAL;
+                                                               else if (p < p1)
+                                                                       return BinarySearch::Comp::LEFT; // min is in decreasing order
+                                                               else
+                                                                       return BinarySearch::Comp::RIGHT;
+                                                       });
+
+                        if (R == -1 || L == -1)
+                                continue;
+
+                        L = min(L + 1, n - 1);
+                        R = min(R + 1, n - 1);
+
+                        int answer = bSearch.firstOccurance(L, R,
+                                                            [&table, &n, &p1](int mid) -> BinarySearch::Comp
+                                                            {
+                                                                    int p = table.query_impodent(mid, n - 1).second;
+                                                                    if (p == p1)
+                                                                            return BinarySearch::Comp::EQUAL;
+                                                                    else if (p < p1)
+                                                                            return BinarySearch::Comp::LEFT; // max is in increasing order
+                                                                    else
+                                                                            return BinarySearch::Comp::RIGHT;
+                                                            });
+
+                        if (answer == -1)
+                                continue;
+
+                        int p2 = table.query_impodent(i + 1, answer - 1).first;
+                        int p3 = table.query_impodent(answer, n - 1).second;
+
+                        if (p3 == p1 && p2 == p1)
                         {
-                                int p2 = query(left + 1, right - 1).first;
-                                if (p2 == p1)
-                                {
-                                        left++, right++;
-                                        break;
-                                }
-                                int notChanged = true;
-
-                                while (left + 1 < right && spTable[0][left + 1].first < p1)
-                                        left++, notChanged = false;
-
-                                while (right - 1 > left && spTable[0][right - 1].first < p1)
-                                        right--, notChanged = false;
-                                if (notChanged)
-                                        left++;
-                                // cout << "left " << left << " right " << right << " p1 " << p1 << " p2 " << p2 << " p3 " << p3 << endl;
+                                found = true;
+                                cout << "YES\n"
+                                     << i + 1 << " " << answer - i - 1 << " " << n - answer << endl;
+                                break;
                         }
                 }
-                if (right <= left + 1)
+
+                if (!found)
                 {
                         // no answer
                         cout << "NO\n";
-                }
-                else
-                {
-                        // x=left,y=right-left+1,z=n-right+1
-                        cout << "YES\n"
-                             << left << " " << right - left - 1 << " " << n - right + 1 << endl;
                 }
         }
         return 0;
